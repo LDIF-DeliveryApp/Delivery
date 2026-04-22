dae6cdc94718dc216679d8f06377ad11cb3d843c
tlsdud135 <kytbible@gmail.com>
2026-04-23 03:37:48 +0900

    refactor: 공통 응답 적용 - menu, ai, storeController(일부) 적용

--- src/main/java/com/ldif/delivery/ai/presentation/controller/AiControllerV1.java
+++ src/main/java/com/ldif/delivery/ai/presentation/controller/AiControllerV1.java
@@ -4,9 +4,11 @@ import com.ldif.delivery.ai.application.service.AiServiceV1;
 import com.ldif.delivery.ai.presentation.dto.AiRequest;
 import com.ldif.delivery.ai.presentation.dto.AiResponse;
 import com.ldif.delivery.global.infrastructure.config.security.UserDetailsImpl;
+import com.ldif.delivery.global.infrastructure.presentation.dto.CommonResponse;
 import com.ldif.delivery.user.domain.entity.UserRoleEnum;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -24,8 +26,8 @@ public class AiControllerV1 {
 
     @PostMapping("/product-description")
     @Secured(UserRoleEnum.Authority.OWNER)
-    public ResponseEntity<AiResponse> setDescription(@Valid @RequestBody AiRequest aiRequest, @AuthenticationPrincipal UserDetailsImpl loginUser) {
-        return ResponseEntity.ok(aiServiceV1.setDescription(aiRequest, loginUser));
+    public ResponseEntity<CommonResponse<AiResponse>> setDescription(@Valid @RequestBody AiRequest aiRequest, @AuthenticationPrincipal UserDetailsImpl loginUser) {
+        return ResponseEntity.status(HttpStatus.OK).body(CommonResponse.success(HttpStatus.OK.value(), "SUCCESS", aiServiceV1.setDescription(aiRequest, loginUser)));
     }
 
 }
--- src/main/java/com/ldif/delivery/menu/presentation/controller/MenuControllerV1.java
+++ src/main/java/com/ldif/delivery/menu/presentation/controller/MenuControllerV1.java
@@ -1,12 +1,14 @@
 package com.ldif.delivery.menu.presentation.controller;
 
 import com.ldif.delivery.global.infrastructure.config.security.UserDetailsImpl;
+import com.ldif.delivery.global.infrastructure.presentation.dto.CommonResponse;
 import com.ldif.delivery.menu.application.service.MenuServiceV1;
 import com.ldif.delivery.menu.presentation.dto.MenuRequest;
 import com.ldif.delivery.menu.presentation.dto.MenuResponse;
 import com.ldif.delivery.user.domain.entity.UserRoleEnum;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -22,26 +24,31 @@ public class MenuControllerV1 {
     private final MenuServiceV1 menuService;
 
     @GetMapping("/{id}")
-    public ResponseEntity<MenuResponse> getMenu(@PathVariable UUID id) {
-        return ResponseEntity.ok(menuService.getMenu(id));
+    public ResponseEntity<CommonResponse<MenuResponse>> getMenu(@PathVariable UUID id) {
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(CommonResponse.success(HttpStatus.OK.value(), "SUCCESS", menuService.getMenu(id)));
     }
 
     @PutMapping("/{id}")
     @Secured({UserRoleEnum.Authority.MASTER, UserRoleEnum.Authority.MANAGER, UserRoleEnum.Authority.OWNER})
-    public ResponseEntity<MenuResponse> updateMenu(@PathVariable UUID id, @Valid @RequestBody MenuRequest request, @AuthenticationPrincipal UserDetailsImpl loginUser) {
-        return ResponseEntity.ok(menuService.updateMenu(id, request, loginUser));
+    public ResponseEntity<CommonResponse<MenuResponse>> updateMenu(@PathVariable UUID id, @Valid @RequestBody MenuRequest request, @AuthenticationPrincipal UserDetailsImpl loginUser) {
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(CommonResponse.success(HttpStatus.OK.value(), "SUCCESS", menuService.updateMenu(id, request, loginUser)));
     }
 
     @DeleteMapping("/{id}")
     @Secured({UserRoleEnum.Authority.MASTER, UserRoleEnum.Authority.OWNER})
-    public void deleteMenu(@PathVariable UUID id, @AuthenticationPrincipal UserDetailsImpl loginUser) {
+    public ResponseEntity<CommonResponse<String>> deleteMenu(@PathVariable UUID id, @AuthenticationPrincipal UserDetailsImpl loginUser) {
         menuService.deleteMenu(id, loginUser);
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(CommonResponse.success(HttpStatus.OK.value(), "SUCCESS", "deleted"));
     }
 
     @PatchMapping("/{id}/hide")
     @Secured({UserRoleEnum.Authority.MASTER, UserRoleEnum.Authority.MANAGER, UserRoleEnum.Authority.OWNER})
-    public ResponseEntity<MenuResponse> hideMenu(@PathVariable UUID id, @AuthenticationPrincipal UserDetailsImpl loginUser) {
-        return ResponseEntity.ok(menuService.hideMenu(id, loginUser));
+    public ResponseEntity<CommonResponse<MenuResponse>> hideMenu(@PathVariable UUID id, @AuthenticationPrincipal UserDetailsImpl loginUser) {
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(CommonResponse.success(HttpStatus.OK.value(), "SUCCESS", menuService.hideMenu(id, loginUser)));
     }
 
 }
