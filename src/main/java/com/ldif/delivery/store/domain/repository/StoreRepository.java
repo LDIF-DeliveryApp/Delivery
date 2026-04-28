4359add371c0430c708321a0fcb1a5f97ae05a6c
rodongwan <rodongwan@burritowallet.com>
2026-04-28 14:46:00 +0900

    feat : 가게 평점 계산 기능 추가 및 retry 기능 추가

--- src/main/java/com/ldif/delivery/store/domain/repository/StoreRepository.java
+++ src/main/java/com/ldif/delivery/store/domain/repository/StoreRepository.java
@@ -1,9 +1,21 @@
 package com.ldif.delivery.store.domain.repository;
 
 import com.ldif.delivery.store.domain.entity.StoreEntity;
+import jakarta.persistence.LockModeType;
+import jakarta.persistence.QueryHint;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.QueryHints;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 import java.util.UUID;
 
 public interface StoreRepository extends JpaRepository<StoreEntity, UUID> {
 
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @QueryHints({@QueryHint(name = "jakarta.persistence.lock.timeout", value = "3000")})
+    @Query("select s from StoreEntity s where s.storeId = :id")
+    Optional<StoreEntity> findByIdWithLock(@Param("id") UUID id);
 }
